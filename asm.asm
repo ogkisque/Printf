f2a4446779045f85ff484b05ee2a6842e99e70d3
og kisque <142596280+ogkisque@users.noreply.github.com>
2024-03-08 16:21:38 +0300

    Add files via upload

--- asm.asm
+++ asm.asm
@@ -2,20 +2,25 @@ section .text
 
 CodeEndStr      equ 0       ; '\0'
 CodePercent     equ 37      ; '%'
+CodeChar        equ 99      ; 'c'
+CodeString      equ 115     ; 's'
 
 global myprintf    
 
-;----------------------------------------
+;--------------------------------------------------------------------------------
 ; Printf (like in c)
 ; Entry:    rsi - format string, ...
 ; Destr:    all
+
 myprintf:  
 
-    sub rsp, 48
-    push rbp
+    pop r10
+    sub rsp, 48                     ; allocate memory for first 6 parameters
+    push r10                        ; shifting the return address
+    push rbp                        ; save rbp
 
     mov rbp, rsp
-    add rbp, 8
+    add rbp, 16
     
     mov [rbp], rdi
     mov [rbp+8], rsi
@@ -24,8 +29,7 @@ myprintf:
     mov [rbp+32], r8
     mov [rbp+40], r9
 
-    mov r10, [rbp]
-    mov [Str], r10
+    mov [Str], rdi
 
     .loop:
         mov rsi, [Str]
@@ -39,22 +43,28 @@ myprintf:
         jmp .loop
 
         .no_percent:      
-        call putchar
+        call putchar_inc_str
 
         jmp .loop
 
     .endloop:
 
     pop rbp
-    add rsp, 48
             
     ret
-;----------------------------------------
+;--------------------------------------------------------------------------------
 ; Parse char after percent
 ; Entry:    
-; Destr:    r10, rsi, r11
+; Destr:    r10, rsi, r11, rbx
 
 parse_percent:
+
+    push rbp
+    mov rbp, rsp
+
+    add rbp, 32
+    add rbp, [CurrOffsArg]          ; [rbp] = current argument
+    mov rbx, rbp
 
     mov r10, [Str]
     inc r10
@@ -66,22 +76,83 @@ parse_percent:
     .check_percent:
         cmp r11b, CodePercent       ; if == '%'
         jne .check_char
-        call putchar
+
+        mov r10, [CurrOffsArg]
+        sub r10, 8
+        mov [CurrOffsArg], r10      ; don't change argument
+        call putchar_inc_str
+
         jmp .exit
 
     .check_char:
+        cmp r11b, CodeChar          ; if == 'c'
+        jne .check_string
 
+        call parse_char
+
+        jmp .exit
+
+    .check_string:
+        cmp r11b, CodeString          ; if == 'c'
+        jne .check_dec
+
+        call parse_string
+
+        jmp .exit
+
+    .check_dec:
 
     .exit:
         
-    
+    mov r10, [CurrOffsArg]
+    add r10, 8
+    mov [CurrOffsArg], r10          ; next argument
+
+    pop rbp
+
     ret
-;----------------------------------------
+;--------------------------------------------------------------------------------
+; Parse %c
+; Entry:    rbx - address of current argument (char)
+; Destr:    
+
+parse_char:
+
+    mov rsi, rbx
+    call putchar_inc_str
+
+    ret
+;--------------------------------------------------------------------------------
+; Parse %s
+; Entry:    rbx - address of current argument (string)
+; Destr:    r10, rsi, r11
+
+parse_string:
+
+    mov rsi, [rbx]
+
+    .loop:
+        mov r11b, byte [rsi]        ; r11b = current char
+        cmp r11b, CodeEndStr        ; if == '\0'
+        je .endloop                 
+    
+        call putchar
+        inc rsi
+
+        jmp .loop
+
+    .endloop:
+    mov r10, [Str]
+    inc r10
+    mov [Str], r10                  ; [Str]++
+
+    ret
+;--------------------------------------------------------------------------------
 ; Print char to stdout, increase [Str]
 ; Entry:    rsi - pointer to char
 ; Destr:    r10
 
-putchar:
+putchar_inc_str:
 
     push rax
     push rdi
@@ -100,8 +171,29 @@ putchar:
     pop rdi
     pop rax
     ret
-;----------------------------------------
+;--------------------------------------------------------------------------------
+; Print char to stdout
+; Entry:    rsi - pointer to char
+; Destr:    
+
+putchar:
+
+    push rax
+    push rdi
+    push rdx
+
+    mov rax, 1
+    mov rdi, 1
+    mov rdx, 1
+    syscall             ; putchar
+
+    pop rdx
+    pop rdi
+    pop rax
+    ret
+;--------------------------------------------------------------------------------
             
 section     .data
             
-Str:        dq 0
+Str:            dq 0
+CurrOffsArg:    dq 8
--- main.cpp
+++ main.cpp
@@ -3,14 +3,14 @@ extern "C" void myprintf (const char* str, ...);
 
 int main ()
 {
-    const char* str1 = "str1\n";
-    const char* str2 = "str2\n";
-    const char* str3 = "str3\n";
-    const char* str4 = "str4\n";
-    const char* str5 = "str5\n";
-    const char* str6 = "str6\n";
-    const char* str7 = "str7\n";
-    const char* str8 = "str8\n";
+    const char* str1 = "format%%\n%c%c\n%c%s%%%c%c\n%s\n";
+    const char str2 = 'a';
+    const char str3 = 'b';
+    const char str4 = 'c';
+    const char* str5 = "\nSTRING\n";
+    const char str6 = 'e';
+    const char str7 = 'f';
+    const char* str8 = "hahahoho";
 
     myprintf (str1, str2, str3, str4, str5, str6, str7, str8);
     return 0;
